74a96c7d8057a421171467f7923d6fb2801820a7
Vung Nguyen <vungnguyen2701@gmail.com>
2023-05-26 15:01:26 +0700

    Todolist_next

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,116 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useState, useRef } from "react"
 
-const inter = Inter({ subsets: ['latin'] })
+
+type Todo = {
+  id: number
+  text: string
+  completed: boolean
+}
 
 export default function Home() {
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [inputValue, setInputValue] = useState<string>('')
+  const [isEdit, setIsEdit] = useState<boolean>()
+  const [editTodo, setEditTodo] = useState<Todo>()
+  
+
+  const handleAddTodo = () => {
+    if(!inputValue.trim()) return
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      text: inputValue,
+      completed: false
+    }
+
+    setTodos([...todos, newTodo])
+    setInputValue("")
+  }
+
+  const handleDeleteTodo = (id:number) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+    setInputValue("")
+    setIsEdit(false)
+  }
+
+  const handleEditTodo = (id:number) => {
+    const newEditTodo = todos.find(todo => todo.id === id)
+
+    if(newEditTodo){
+      setInputValue(newEditTodo.text)
+      setEditTodo(newEditTodo)
+      setIsEdit(true)
+    }
+  }
+
+  const handleSaveEditTodo = () =>{
+    if(!inputValue.trim()) return
+
+    if(editTodo){
+      setTodos(todos.map(todo => todo.id === editTodo.id ? {...todo, text: inputValue} : todo))
+      setInputValue("")
+      setIsEdit(false)
+    }
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className="container mx-auto py-6">
+      <h1 className="text-2xl font-bold mb-4 text-center">TodoList !!!</h1>
+      <div className="flex flex-col justify-center w-full">
+        <input 
+          type="text" 
+          className="border border-gray-400 rounded px-2 py-1"
+          placeholder="Input a new todo..."
+          value={inputValue}
+          onChange={e => setInputValue(e.target.value)}
         />
+        {isEdit
+        ?(<button 
+          className="bg-gray-800 hover:bg-gray-700 text-white px-4 py-1 rounded"
+          onClick={() => handleSaveEditTodo()}
+        >
+          Save Edit
+        </button>)
+        :(<button 
+          className="bg-gray-800 hover:bg-gray-700 text-white px-4 py-1 rounded"
+          onClick={handleAddTodo}
+        >
+          Add Todo
+        </button>)
+        }
+        
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <table className="min-w-full divide-y divide-gray-200 mt-4 border">
+        <thead>
+          <tr>
+            <th className="px-6 py-3 text-left text-gray-800 uppercase tracking-wider">ID</th>
+            <th className="px-6 py-3 text-left text-gray-800 uppercase tracking-wider">Work</th>
+            <th className="px-6 py-3 text-left text-gray-800 uppercase tracking-wider">Status</th>
+            <th className="px-6 py-3 text-left text-gray-800 uppercase tracking-wider" colSpan={2}>Action</th>
+          </tr>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+            {!todos.length
+              ? (<tr><td className="text-center" colSpan={5}>List is empty.</td></tr>)
+              : todos.map(todo => (
+                <tr key={todo.id}>
+                  <td  onClick={() => handleEditTodo(todo.id)} className="px-6 py-4 font-bold">{todo.id}</td>
+                  <td className="px-6 py-4">{todo.text}</td>
+                  {todo.completed 
+                  ? <td className="px-6 py-4">Done</td>
+                  : <td className="px-6 py-4">Pending</td>
+                  }
+                  
+                  <td className="px-6 py-4">
+                    <button onClick={() => handleChange} className="px-6 py-2 rounded bg-green-400 text-white font-bold uppercase">Toggle</button>
+                  </td>
+                  <td className="px-6 py-4">
+                    <button onClick={() => handleDeleteTodo(todo.id)} className="px-6 py-2 rounded bg-red-400 text-white">Delete</button>
+                  </td>
+                </tr>
+              ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
