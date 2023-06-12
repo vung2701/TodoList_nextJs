a7b3bb78e586f3982352b5b7a5b38a54d55b443d
vung2701 <vungnguyen2701@gmail.com>
2023-06-12 15:47:57 +0700

    fix

--- untils/api.ts
+++ untils/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? "https://todo-list-next-js-chi.vercel.app" : 'http://localhost:3000',
+  baseURL: '/',
   // other options
 });
 
