d15b6538944b1e78ed9ed97a582ba3dc3f3e7f78
vung2701 <vungnguyen2701@gmail.com>
2023-06-12 15:27:15 +0700

    fix URL 2

--- untils/api.ts
+++ untils/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? window.location.origin : 'http://localhost:3000',
+  baseURL: process.env.NODE_ENV === 'production' ? process.env.BASE_URL : 'http://localhost:3000',
   // other options
 });
 
